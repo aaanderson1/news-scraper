42bdb0b91e8266495dc0d30e3abcabe591da1e12
Allison <andersonaallison@gmail.com>
2019-07-22 20:19:27 -0600

    web scraping fetch

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,11 @@
 const friends = require('../data/friends.js');
 
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 
 module.exports = {};
 
@@ -8,14 +14,12 @@ module.exports.getHeadlinesRoute = (app) => {
         const data = req.query;
         console.log(data);
         res.setHeader('Content-Type', 'application/json');
-        const headlines = [
-            {
-                headline: "text", 
-                url: "google.com",
-                summary: "blah",
-                _id: 0
-            }
-        ];
+        const headlines = [{
+            headline: "text",
+            url: "google.com",
+            summary: "blah",
+            _id: 0
+        }];
         res.end(JSON.stringify(headlines));
     });
 };
@@ -23,7 +27,30 @@ module.exports.getHeadlinesRoute = (app) => {
 module.exports.getFetchRoute = (app) => {
     app.get('/api/fetch', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(""));
+        // First, we grab the body of the html with axios
+        axios.get("https://www.theonion.com/").then(function (response) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(response.data);
+
+            // Now, we grab every h2 within an article tag, and do the following:
+            $("article h1").each(function (i, element) {
+                // Save an empty result object
+                var result = {};
+
+                // Add the text and href of every link, and save them as properties of the result object
+                result.title = $(this)
+                    .parent("a")
+                    .text();
+                result.link = $(this)
+                    .parent("a")
+                    .attr("href");
+
+                // Create a new Article using the `result` object built from scraping
+console.log(`title: ${result.title}, link: ${result.link}`);
+            });
+
+            // Send a message to the client
+            res.send("Scrape Complete");
+        });
     });
 };
-
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'))
-
+//connect handlebars to express app
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -18,3 +18,18 @@ htmlRoutes.savedRoute(app);
 apiRoutes.getHeadlinesRoute(app);
 apiRoutes.getFetchRoute(app);
 app.listen(PORT);
+
+// //if deployed use the deployed database, otherwise use the local mongoHeadlines database
+// var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// //connect mongoose to our database
+// mongoose.connect(db, function(error){
+//     //log any errors connecting with mongoose
+//     if (error) {
+//         console.log(error);
+//     }
+//     //or log a success message
+//     else {
+//         console.log("mongoose connection is successful");
+//     }
+// });
